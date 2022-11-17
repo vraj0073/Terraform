d0466f6a22989a33e07114f65a9e26d6c99120a7
Vraj Bhavesh Jadhav <vrajjadhav0073@gmail.com>
2022-11-17 14:42:01 -0400

    Cluster of servers using load balancer

--- main.tf
+++ main.tf
@@ -1,70 +1,152 @@
-
 provider "aws" {
-region = "us-east-1"
+
+    region = "us-east-1"
+
 }
 
-variable "instance_port" {
-
-    type = number
-    default = 8080
+output "alb_dns_name" {
+value = aws_lb.load.dns_name
+description = "The domain name of the load balancer"
 }
 
-data "aws_vpc" "default" {
-    default = true
+variable "server_port" {
+description = "The port the server will use for HTTP requests"
+type = number
+default = 8080
 }
 
-data "aws_subnets" "default" {
+data "aws_vpc" "example" {
+    default = "true"
+}
+
+data "aws_subnets" "subnet" {
 
     filter{
 
         name = "vpc-id"
-        values = [data.aws_vpc.default.id]
+     values = [data.aws_vpc.example.id]
     }
 }
 
-
-
-resource "aws_launch_configuration" "instance"{
+resource "aws_launch_configuration" "launch" {
 
     image_id = "ami-09d3b3274b6c5d4aa"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.instance-security.id]
-   
-    user_data = <<-EOF
-                echo "Hello" > index.xhtml
-                nohup busybox httpd -f -p ${var.instance_port} &
-                EOF     
+    security_groups = [aws_security_group.autosacling.id]
 
-    
-
-    lifecycle {
-create_before_destroy = true
-}
-    
+     lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_autoscaling_group" "example" {
+resource "aws_security_group" "autosacling" {
+    name = "autosacling"
+    
+    ingress{
+to_port = 80
+from_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 
-    launch_configuration = aws_launch_configuration.instance.name
-    vpc_zone_identifier = data.aws_subnets.default.ids
-    min_size = 2
+    }
+}
+resource "aws_autoscaling_group" "auto"{
+
+    launch_configuration = aws_launch_configuration.launch.name
     max_size = 10
+    min_size = 2
+    vpc_zone_identifier = data.aws_subnets.subnet.ids
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
 
-    tag {
+     tag {
 key = "Name"
-value = "terraform-asg-example"
+value = "terraform-aws-autosacling"
 propagate_at_launch = true
 }
 }
 
-resource "aws_security_group" "instance-security" {
-    name = "terraform security"
+resource "aws_lb" "load" {
+    name = "test-load"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.subnet.ids
+    security_groups = [aws_security_group.lb-listner.id]
+}
 
-    ingress {
+resource "aws_security_group" "lb-listner" {
 
-        to_port = var.instance_port
-        from_port = var.instance_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        name = "terraform-alb"
+
+        ingress{
+            to_port = 80
+            from_port = 80
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress{
+            to_port = 0
+            from_port = 0 
+            protocol = "-1"
+            cidr_blocks = ["0.0.0.0/0"] 
+
+        }
+}
+
+resource "aws_lb_listener" "http" {
+
+    load_balancer_arn = aws_lb.load.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+
+        type = "fixed-response"
+
+        fixed_response{
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+
+        }
     }
 }
+
+resource "aws_lb_target_group" "asg" {
+
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.example.id
+
+    health_check {
+
+        path = "/"
+protocol = "HTTP"
+matcher = "200"
+interval = 15
+timeout = 3
+healthy_threshold = 2
+unhealthy_threshold = 2
+
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    condition {
+path_pattern {
+values = ["*"]
+}
+}
+action {
+type = "forward"
+target_group_arn = aws_lb_target_group.asg.arn
+}
+
+}
+
+
